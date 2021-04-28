c21d76f6419dbd3a1793f32862b7569c6eb111f1
binkang666 <54644269+binkang666@users.noreply.github.com>
2021-04-28 06:55:00 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,9 @@
-from Vendors import vendors
-from tkinter import *
+'''from Vendors import vendors'''
 from tkinter import ttk
 import tkinter as tk
-import sqlite3
-from Dashboard import Dashboard
 from frontEnd_V0_3 import MainScreen
-
-running = True
-
-def on_closing():
-    running = False
+from report import plot
+from tkinter import *
 
 root = tk.Tk()
 root.title('CECS 445 Inventory Manager By Team Boba')
@@ -17,20 +11,43 @@ root.title('CECS 445 Inventory Manager By Team Boba')
 ws = root.winfo_screenwidth()
 hs = root.winfo_screenheight()
 x = (ws / 2) - (1400 / 2)
-y = (hs / 2) - (700 / 2)
-root.geometry('%dx%d+%d+%d' % (1400, 700, x, y))
+y = (hs / 2) - (1000 / 2)
+root.geometry('%dx%d+%d+%d' % (1400, 1000, x, y))
 tabControl = ttk.Notebook(root)
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)
+
 
 tabControl.add(tab1, text="Main Tab")
-tabControl.add(tab2, text="Dashboard")
-tabControl.add(tab3, text="Vendors")
+tabControl.add(tab2, text="Monthly Reports")
 tabControl.grid(row=0, column=0)
 MainScreen(tab1,root)
-Dashboard(tab2)
-vendors(tab3)
+plot(tab2)
+style = ttk.Style(root)
+style.theme_use("winnative")
+style.configure('Treeview', rowheight=34)
+style.configure("Treeview.Heading", foreground='green', background='light gray', relief="flat" ,font=("Helvetica",11,"bold"))
 
+'''global selected
+selected = False
+def cut_text(e):
+    global selected
+    if my_text.secletion_get():
+        selected = my_text.secletion_get()
+        my_text.delete("sel.first", "sel.last")
+
+def copy_text(e):
+    global selected
+    if my_text.secletion_get():
+        selected = my_text.secletion_get()
+
+def paste_text(e):
+    if selected:
+        position = my_text.index(INSERT)
+        my_text.insert(position, selected)
+
+root.bing('<Control-Key-x>', cut_text)
+root.bing('<Control-Key-c>', copy_text)
+root.bing('<Control-Key-v>', paste_text)'''
 root.mainloop()
 
