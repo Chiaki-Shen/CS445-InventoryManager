df9e6c97251b60f994e4cc9e8bdfd2b268f5f726
binkang666 <54644269+binkang666@users.noreply.github.com>
2021-04-29 00:29:31 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
 '''from Vendors import vendors'''
+import sqlite3
 from tkinter import ttk
 import tkinter as tk
 from frontEnd_V0_3 import MainScreen
-'''from report import plot'''
+from report import plot
 from tkinter import *
+from shopify_scraper import *
+from PopupMenu import Popup
+
 
 root = tk.Tk()
 root.title('CECS 445 Inventory Manager By Team Boba')
@@ -22,32 +26,23 @@ tabControl.add(tab1, text="Main Tab")
 tabControl.add(tab2, text="Monthly Reports")
 tabControl.grid(row=0, column=0)
 MainScreen(tab1,root)
-'''plot(tab2)'''
+plot(tab2)
 style = ttk.Style(root)
 style.theme_use("winnative")
 style.configure('Treeview', rowheight=34)
 style.configure("Treeview.Heading", foreground='green', background='light gray', relief="flat" ,font=("Helvetica",11,"bold"))
 
-'''global selected
-selected = False
-def cut_text(e):
-    global selected
-    if my_text.secletion_get():
-        selected = my_text.secletion_get()
-        my_text.delete("sel.first", "sel.last")
+conn = sqlite3.connect('Hiccups.db')  # create a DB if there is not one
 
-def copy_text(e):
-    global selected
-    if my_text.secletion_get():
-        selected = my_text.secletion_get()
 
-def paste_text(e):
-    if selected:
-        position = my_text.index(INSERT)
-        my_text.insert(position, selected)
+style.map('Treeview', background = [('selected','green')])
+my_tree = ttk.Treeview(root)
 
-root.bing('<Control-Key-x>', cut_text)
-root.bing('<Control-Key-c>', copy_text)
-root.bing('<Control-Key-v>', paste_text)'''
+
+
+
+
+
+
 root.mainloop()
 
